2ab1de06ec07f2e4a5f478b4fd93c12a255501c8
Laura-Moya <l.moya@alumnos.upm.es>
2020-12-08 18:09:28 +0100

    Prove con la navbar. (Abbiamo aggiunto la nuova search bar)

--- common/navbar.php
+++ common/navbar.php
@@ -1,68 +1,46 @@
 <!-- Navbar -->
-<nav class="navbar navbar-expand-sm navbar-light" id="prima_navbar">
+<nav class="navbar navbar-expand-sm navbar-light" id="prima-navbar">
 
-    <div class='container'>
-        <div class='row'>
-          <table>
-                <tr>
-                  <div class="col-sm-12 col-md-3 col-lg-3">
-                  <td>
+    <div class="col-sm-12 col-md-3 col-lg-3">
+      <a class="navbar-brand" href="index.php"> <img src="./images/logo.png" style="width: 15rem;" id="logo" alt="logo"></a>
+    </div>
 
-                    <a class="navbar-brand" href="index.php"> <img src="./images/logo.png" style="width: 15rem;" id="logo" alt="logo"> </a>
+    <div class="col-sm-6 col-md-6 col-lg-6">
 
-                  </td>
-                  </div>
-                  <div class="col-sm-6 col-md-6 col-lg-6">
-
-                <td>
-
-              <form class='navbar-form'>
-                <div class='input-group'>
-                  <input type="text" placeholder="Search..">
-
-                  <div class="">
-                    <button type='submit' class='btn btn-default'>
-                      <i class="fas fa-search"></i>
-                    </button>
-                  </div>
-                </div>
-              </form>
-          </td>
-        </div>
-        <div class="col-sm-6 col-md-3 col-lg-3">
-          <td>
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <!-- LOGIN -->
-                  <a class="nav-link" onclick="openForm()"><i class="fas fa-sign-in-alt"></i>Accedi</a>
-                    <div class="form-popup container-registrazione" id="myForm">
-                      <form action="./check.php" method="POST">
-                      <h4>Il Tuo Mercatino Online</h4>
-
-                      <label for="email"><b>Email</b></label> <br/>
-                      <input type="text" placeholder="Immettere Email" name="email" required><br/>
-
-                      <label for="psw"><b>Password</b></label> <br/>
-                      <input type="password" placeholder="Immettere Password" name="password" required> <br/>
-                      <p>Non hai un account? <a href="./registrazione.php">Registrati!</a></p>
-
-                      <button type="submit" class="btn btn-primary btn-login">Accedi</button>
-                      <button type="button" class="btn btn-primary btn-login" onclick="closeForm()">Chiudi</button>
-                    </form>
-                  </div>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#"><i class="fas fa-eye fa-lg icon-eye"></i></a>
-                </li>
-              </ul>
-
-          </td>
-          </div>
-        </tr>
-        </div>
-      </table>
+    <form class="example" action="/action_page.php" style="margin:auto;max-width:300px">
+      <input type="text" placeholder="Cerca..." name="search2">
+      <button type="submit"><i class="fa fa-search"></i></button>
+    </form>
 
     </div>
+
+    <div class="col-sm-6 col-md-3 col-lg-3">
+      <ul class="navbar-nav">
+                          <li class="nav-item">
+                            <!-- LOGIN -->
+                            <a class="nav-link" onclick="openForm()"><i class="fas fa-sign-in-alt"></i>Accedi</a>
+                              <div class="form-popup container-registrazione" id="myForm">
+                                <form action="./check.php" method="POST">
+                                <h4>Il Tuo Mercatino Online</h4>
+
+                                <label for="email"><b>Email</b></label> <br/>
+                                <input type="text" placeholder="Immettere Email" name="email" required><br/>
+
+                                <label for="psw"><b>Password</b></label> <br/>
+                                <input type="password" placeholder="Immettere Password" name="password" required> <br/>
+                                <p>Non hai un account? <a href="./registrazione.php">Registrati!</a></p>
+
+                                <button type="submit" class="btn btn-primary btn-login">Accedi</button>
+                                <button type="button" class="btn btn-primary btn-login" onclick="closeForm()">Chiudi</button>
+                              </form>
+                            </div>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fas fa-eye fa-lg icon-eye"></i></a>
+                          </li>
+                        </ul>
+    </div>
+
 </nav>
 <!-- Indirizzo e Categorie -->
 <nav class="navbar navbar-expand-lg navbar-light">
