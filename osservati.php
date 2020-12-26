cc1d5ecffc352101b77ad6a383ee550c3f9f9276
Laura-Moya <l.moya@alumnos.upm.es>
2020-12-26 21:50:22 +0100

    osservati

---
+++ osservati.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+  <?php include "common/header.php";?>
+
+<body>
+  <?php include "common/navbar.php";?>
+
+  <!-- I miei prodotti osservati -->
+  <div class="container">
+    <p id="primo-piano">I tuoi prodotti osservati</p>
+    <div class="row">
+      <div class="card" style="width: 16rem;">
+        <img src="images/fornellino.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      <div class="card" style="width: 16rem;">
+        <img src="images/fornellino.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      <div class="card" style="width: 16rem;">
+        <img src="images/fornellino.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      <div class="card" style="width: 16rem;">
+        <img src="images/fornellino.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script>
+
+    function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    }
+
+    function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    }
+
+  </script>
+</body>
+<!-- include "common/footer.php"; -->
+</html>
