4a91b278061ff39184c39e6f4b7b05b890dee5b9
valenis96 <valentina@maronese.com>
2021-01-25 16:47:08 +0100

    Pagamento funziona!!!

--- pagamento.php
+++ pagamento.php
@@ -19,33 +19,40 @@
           <p>Venditore: Valentina Maronese</p>
           <div class="stessa-riga" >
           <h6>Preferisce:</h6>
-          <form>
-            <input type = "radio" id = "B" name ="AA"/>
+            <input type = "radio" id = "B" name ="AA" checked="checked"/>
             <label style="margin-right:0.5rem" for = "B" > Spedizione </label>
             <input type = "radio" id ="A" name ="AA"/>
             <label style="margin-right:0.5rem" for = "A"> Ritiro a mano </label>
 
-            <div class="paga">
+            <div class="paga" >
               <h6 style="margin-top: 1rem;">Come vuoi pagare?</h6>
-              <input type="radio"  id="C" name="P"/>
-              <label style="margin-right:0.5rem" for="C">Contanti</label>
-              <input type="radio"  id="CA" name="P"/>
-              <label style="margin-right:0.5rem" for="CA">Carta di pagamento</label>
+              <input type="radio" name="P" onclick="closeDiv()" checked="checked"/>
+              <label style="margin-right:0.5rem">Contanti</label>
+              <input type="radio" name="P"onclick="openDiv()"/>
+              <label style="margin-right:0.5rem">Carta di pagamento</label>
             </div>
 
-              <div class="cartacredito">
+            <div id="cartacredito" style= "display:none">
                 <h6 style="margin-top: 1rem;">Inserisce i dati de la sua carta</h6>
                 <input type="text" name="intestatario" placeholder="Nome e cognome..."/><br/>
                 <input type="number" maxlength="16" name="numero_carta" placeholder="Numero di carta..."/><br/>
                 <input type="text" maxlength="5" name="data_scadenza" placeholder="mm/aa"/><br/>
                 <input type="number" maxlength="3" name="cvv" placeholder="CVV..."/>
             </div>
-          </form>
           <h2 id="prezzo" >Prezzo: 87.16€</h2>
           <button class="btn btn-primary" type="button">Acquista ora</button>
         </div>
       </div>
     </div>
+    <script>
+      function openDiv() {
+        document.getElementById("cartacredito").style.display = "block";
+      }
+
+      function closeDiv() {
+        document.getElementById("cartacredito").style.display = "none";
+      }
+    </script>
 
 
   </body>
