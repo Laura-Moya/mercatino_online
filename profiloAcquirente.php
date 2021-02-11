71377634e286ea681a668b8421491994082f2a71
Laura-Moya <l.moya@alumnos.upm.es>
2021-02-11 13:04:54 +0100

    Link di diventa venditore di colore bianco

--- profiloAcquirente.php
+++ profiloAcquirente.php
@@ -1,6 +1,6 @@
     <?php include 'common/profilo.php';?>
         <form class="" action="diventaVenditore.php" method="post">
-          <button class="btn btn-primary" type="submit"><a href="diventaVenditore.php?codiceFiscale=<?php echo $codice_fiscale[0];?>">Diventa venditore!</a> </button>
+          <button class="btn btn-primary" type="submit"><a style="color: white !important;" href="diventaVenditore.php?codiceFiscale=<?php echo $codice_fiscale[0];?>">Diventa venditore!</a> </button>
         </form>
 
 
